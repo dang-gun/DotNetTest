cd7f8a7e302de28ae7c628b728ff6ae23bf1209a
최영동(y&j) <c1dong8@nate.com>
2022-03-11 19:53:59 +0900

    NFC 테스트 프로젝트 추가

---
+++ NfcTest/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace NfcTest
+{
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.comboCardList = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.btnCardListRefresh = new System.Windows.Forms.Button();
+			this.listLog = new System.Windows.Forms.ListView();
+			this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+			this.btnSendApdu = new System.Windows.Forms.Button();
+			this.btnReadReaderAttr = new System.Windows.Forms.Button();
+			this.btnGetData = new System.Windows.Forms.Button();
+			this.btnSetData = new System.Windows.Forms.Button();
+			this.btnAuthBlock1 = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.txtUpdateBinary = new System.Windows.Forms.TextBox();
+			this.btnBlock0UpdateBinary = new System.Windows.Forms.Button();
+			this.btnBlock0ReadBinary = new System.Windows.Forms.Button();
+			this.labInfo = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+			this.label2 = new System.Windows.Forms.Label();
+			this.btnBlockAuth = new System.Windows.Forms.Button();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// comboCardList
+			// 
+			this.comboCardList.FormattingEnabled = true;
+			this.comboCardList.Location = new System.Drawing.Point(118, 10);
+			this.comboCardList.Name = "comboCardList";
+			this.comboCardList.Size = new System.Drawing.Size(471, 23);
+			this.comboCardList.TabIndex = 0;
+			this.comboCardList.SelectedIndexChanged += new System.EventHandler(this.comboCardList_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Card List";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// btnCardListRefresh
+			// 
+			this.btnCardListRefresh.Location = new System.Drawing.Point(597, 10);
+			this.btnCardListRefresh.Name = "btnCardListRefresh";
+			this.btnCardListRefresh.Size = new System.Drawing.Size(100, 23);
+			this.btnCardListRefresh.TabIndex = 2;
+			this.btnCardListRefresh.Text = "Refresh";
+			this.btnCardListRefresh.UseVisualStyleBackColor = true;
+			this.btnCardListRefresh.Click += new System.EventHandler(this.btnCardListRefresh_Click);
+			// 
+			// listLog
+			// 
+			this.listLog.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+			this.listLog.HideSelection = false;
+			this.listLog.Location = new System.Drawing.Point(12, 228);
+			this.listLog.Name = "listLog";
+			this.listLog.Size = new System.Drawing.Size(685, 350);
+			this.listLog.TabIndex = 3;
+			this.listLog.UseCompatibleStateImageBehavior = false;
+			this.listLog.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "Time";
+			this.columnHeader1.Width = 80;
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Contents";
+			this.columnHeader2.Width = 580;
+			// 
+			// btnSendApdu
+			// 
+			this.btnSendApdu.Location = new System.Drawing.Point(12, 50);
+			this.btnSendApdu.Name = "btnSendApdu";
+			this.btnSendApdu.Size = new System.Drawing.Size(160, 23);
+			this.btnSendApdu.TabIndex = 4;
+			this.btnSendApdu.Text = "Send ISO7816 APDUs";
+			this.btnSendApdu.UseVisualStyleBackColor = true;
+			this.btnSendApdu.Click += new System.EventHandler(this.btnSendApdu_Click);
+			// 
+			// btnReadReaderAttr
+			// 
+			this.btnReadReaderAttr.Location = new System.Drawing.Point(12, 79);
+			this.btnReadReaderAttr.Name = "btnReadReaderAttr";
+			this.btnReadReaderAttr.Size = new System.Drawing.Size(160, 23);
+			this.btnReadReaderAttr.TabIndex = 5;
+			this.btnReadReaderAttr.Text = "Read reader attributes";
+			this.btnReadReaderAttr.UseVisualStyleBackColor = true;
+			this.btnReadReaderAttr.Click += new System.EventHandler(this.btnReadReaderAttr_Click);
+			// 
+			// btnGetData
+			// 
+			this.btnGetData.Location = new System.Drawing.Point(12, 108);
+			this.btnGetData.Name = "btnGetData";
+			this.btnGetData.Size = new System.Drawing.Size(160, 23);
+			this.btnGetData.TabIndex = 6;
+			this.btnGetData.Text = "Transmit - GetData";
+			this.btnGetData.UseVisualStyleBackColor = true;
+			this.btnGetData.Click += new System.EventHandler(this.btnGetData_Click);
+			// 
+			// btnSetData
+			// 
+			this.btnSetData.Location = new System.Drawing.Point(12, 137);
+			this.btnSetData.Name = "btnSetData";
+			this.btnSetData.Size = new System.Drawing.Size(160, 23);
+			this.btnSetData.TabIndex = 7;
+			this.btnSetData.Text = "Transmit - SetData";
+			this.btnSetData.UseVisualStyleBackColor = true;
+			this.btnSetData.Click += new System.EventHandler(this.btnSetData_Click);
+			// 
+			// btnAuthBlock1
+			// 
+			this.btnAuthBlock1.Location = new System.Drawing.Point(6, 29);
+			this.btnAuthBlock1.Name = "btnAuthBlock1";
+			this.btnAuthBlock1.Size = new System.Drawing.Size(160, 23);
+			this.btnAuthBlock1.TabIndex = 8;
+			this.btnAuthBlock1.Text = "Block 0 - Auth ";
+			this.btnAuthBlock1.UseVisualStyleBackColor = true;
+			this.btnAuthBlock1.Click += new System.EventHandler(this.btnAuthBlock1_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.txtUpdateBinary);
+			this.groupBox1.Controls.Add(this.btnBlock0UpdateBinary);
+			this.groupBox1.Controls.Add(this.btnBlock0ReadBinary);
+			this.groupBox1.Controls.Add(this.btnAuthBlock1);
+			this.groupBox1.Location = new System.Drawing.Point(178, 50);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(182, 172);
+			this.groupBox1.TabIndex = 9;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Custom Command";
+			// 
+			// txtUpdateBinary
+			// 
+			this.txtUpdateBinary.Location = new System.Drawing.Point(6, 116);
+			this.txtUpdateBinary.Name = "txtUpdateBinary";
+			this.txtUpdateBinary.Size = new System.Drawing.Size(160, 23);
+			this.txtUpdateBinary.TabIndex = 11;
+			// 
+			// btnBlock0UpdateBinary
+			// 
+			this.btnBlock0UpdateBinary.Location = new System.Drawing.Point(6, 87);
+			this.btnBlock0UpdateBinary.Name = "btnBlock0UpdateBinary";
+			this.btnBlock0UpdateBinary.Size = new System.Drawing.Size(160, 23);
+			this.btnBlock0UpdateBinary.TabIndex = 10;
+			this.btnBlock0UpdateBinary.Text = "Block 0 - Update Binary";
+			this.btnBlock0UpdateBinary.UseVisualStyleBackColor = true;
+			this.btnBlock0UpdateBinary.Click += new System.EventHandler(this.btnBlock0UpdateBinary_Click);
+			// 
+			// btnBlock0ReadBinary
+			// 
+			this.btnBlock0ReadBinary.Location = new System.Drawing.Point(6, 58);
+			this.btnBlock0ReadBinary.Name = "btnBlock0ReadBinary";
+			this.btnBlock0ReadBinary.Size = new System.Drawing.Size(160, 23);
+			this.btnBlock0ReadBinary.TabIndex = 9;
+			this.btnBlock0ReadBinary.Text = "Block 0 - Read Binary";
+			this.btnBlock0ReadBinary.UseVisualStyleBackColor = true;
+			this.btnBlock0ReadBinary.Click += new System.EventHandler(this.btnBlock0ReadBinary_Click);
+			// 
+			// labInfo
+			// 
+			this.labInfo.Location = new System.Drawing.Point(597, 36);
+			this.labInfo.Name = "labInfo";
+			this.labInfo.Size = new System.Drawing.Size(100, 189);
+			this.labInfo.TabIndex = 10;
+			this.labInfo.Text = "Card List";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.btnBlockAuth);
+			this.groupBox2.Controls.Add(this.label2);
+			this.groupBox2.Controls.Add(this.numericUpDown1);
+			this.groupBox2.Location = new System.Drawing.Point(366, 50);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(174, 172);
+			this.groupBox2.TabIndex = 11;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "Block Command";
+			// 
+			// numericUpDown1
+			// 
+			this.numericUpDown1.Location = new System.Drawing.Point(112, 22);
+			this.numericUpDown1.Name = "numericUpDown1";
+			this.numericUpDown1.Size = new System.Drawing.Size(54, 23);
+			this.numericUpDown1.TabIndex = 0;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(6, 22);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Use Block";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+			// 
+			// btnBlockAuth
+			// 
+			this.btnBlockAuth.Location = new System.Drawing.Point(6, 51);
+			this.btnBlockAuth.Name = "btnBlockAuth";
+			this.btnBlockAuth.Size = new System.Drawing.Size(160, 23);
+			this.btnBlockAuth.TabIndex = 3;
+			this.btnBlockAuth.Text = "Block Auth";
+			this.btnBlockAuth.UseVisualStyleBackColor = true;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(710, 590);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.labInfo);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.btnSetData);
+			this.Controls.Add(this.btnGetData);
+			this.Controls.Add(this.btnReadReaderAttr);
+			this.Controls.Add(this.btnSendApdu);
+			this.Controls.Add(this.listLog);
+			this.Controls.Add(this.btnCardListRefresh);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboCardList);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox comboCardList;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button btnCardListRefresh;
+		private System.Windows.Forms.ListView listLog;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.Button btnSendApdu;
+		private System.Windows.Forms.Button btnReadReaderAttr;
+		private System.Windows.Forms.Button btnGetData;
+		private System.Windows.Forms.Button btnSetData;
+		private System.Windows.Forms.Button btnAuthBlock1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label labInfo;
+		private System.Windows.Forms.Button btnBlock0ReadBinary;
+		private System.Windows.Forms.Button btnBlock0UpdateBinary;
+		private System.Windows.Forms.TextBox txtUpdateBinary;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Button btnBlockAuth;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.NumericUpDown numericUpDown1;
+	}
+}
---
+++ NfcTest/Form1.cs
@@ -0,0 +1,405 @@
+﻿using PCSC;
+using PCSC.Iso7816;
+using PCSC.Monitoring;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NfcTest
+{
+	public partial class Form1 : Form
+	{
+		NfcReader m_nfc;
+
+		IMonitorFactory monitorFactory;
+		ISCardMonitor monitor;
+
+		private const byte MSB = 0x00;
+		private const byte LSB = 0x0A;
+
+
+		public Form1()
+		{
+			InitializeComponent();
+
+			this.m_nfc = new NfcReader();
+
+			//인스턴스 생성
+			monitorFactory = MonitorFactory.Instance;
+			monitor = monitorFactory.Create(SCardScope.System);
+			//이벤트 연결
+			monitor.StatusChanged += Monitor_StatusChanged;
+
+			//카드 리스트 새로고침
+			btnCardListRefresh_Click(null, null);
+		}
+
+		private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			this.m_nfc.Dispose();
+			monitor.Cancel();
+			monitor.Dispose();
+		}
+
+		/// <summary>
+		/// 로그 추가
+		/// </summary>
+		/// <param name="sMessage"></param>
+		private void LogAdd(string sMessage)
+		{
+			if (true == InvokeRequired)
+			{//다른 쓰래드다.
+				this.Invoke(new Action(
+					delegate ()
+					{
+						this.LogAddNotInvoke(sMessage);
+					}));
+			}
+			else
+			{//같은 쓰래드다.
+				this.LogAddNotInvoke(sMessage);
+			}
+
+		}
+
+		private void LogAddNotInvoke(string sMessage)
+		{
+			ListViewItem item = new ListViewItem();
+			item.Text = DateTime.Now.ToString("HH:mm:ss");
+			item.SubItems.Add(sMessage);
+
+			this.listLog.Items.Add(item);
+			this.listLog.Items[this.listLog.Items.Count - 1].EnsureVisible();
+		}
+
+
+		private void comboCardList_SelectedIndexChanged(object sender, EventArgs e)
+		{
+
+			this.m_nfc.ReaderNameSet(comboCardList.Text);
+
+			if (string.Empty != comboCardList.Text)
+			{
+				//모니터링 시작
+				this.monitor.Start(this.m_nfc.ReaderName);
+				//리더기 정보 출력
+				this.btnReadReaderAttr_Click(null, null);
+			}
+			else
+			{
+				this.m_nfc.ReaderNameSet(string.Empty);
+				this.LogAdd("please Select Reader");
+			}
+			
+		}
+
+		private void Monitor_StatusChanged(object sender, StatusChangeEventArgs e)
+		{
+			StringBuilder sbTemp = new StringBuilder();
+			StringBuilder sbTemp2 = new StringBuilder();
+			sbTemp.Append(">> Status Changed : ");
+
+			if (e.NewState.HasFlag(SCRState.Present))
+			{
+				sbTemp2.Append(", Card inserted(카드 들어있음)");
+			}
+			if (e.NewState.HasFlag(SCRState.InUse))
+			{
+				sbTemp2.Append(", In use(사용중, 공유 모드일 가능성 있음)");
+			}
+			if (e.NewState.HasFlag(SCRState.Empty))
+			{
+				sbTemp2.Append(", Card removed(비어 있음)");
+			}
+
+			if (0 == sbTemp2.Length)
+			{
+				sbTemp2.Append(e.NewState);
+			}
+
+			this.LogAdd(sbTemp.ToString() + sbTemp2.ToString());
+		}
+
+		/// <summary>
+		/// 카드리스트 새로고침 버튼
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnCardListRefresh_Click(object? sender, EventArgs? e)
+		{
+			string[] sNameList = this.m_nfc.ReaderList();
+			IContextFactory contextFactory = ContextFactory.Instance;
+
+			//기존 리스트를 지우고
+			comboCardList.Items.Clear();
+
+			//새 리스트를 만든다.
+			foreach (string itemName in sNameList)
+			{
+				comboCardList.Items.Add(itemName);
+				this.LogAdd("find Reader : " + itemName);
+			}
+		}
+
+
+		/// <summary>
+		/// ATR 정보 출력
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnReadReaderAttr_Click(object? sender, EventArgs? e)
+		{
+
+			byte[]? byteAtr = this.m_nfc.CardAttributeGet(SCardAttribute.AtrString);
+			string sData = string.Empty;
+
+			if (null != byteAtr)
+			{
+				sData = BitConverter.ToString(byteAtr);
+				this.LogAdd(string.Format("ATR: {0}", sData));
+			}
+			else
+			{
+				this.LogAdd(string.Format("ATR: Failure"));
+			}
+		}
+
+		/// <summary>
+		/// 임의의 Apdu명령을 날려본다
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnSendApdu_Click(object sender, EventArgs e)
+		{
+			var contextFactory = ContextFactory.Instance;
+			using (var ctx = contextFactory.Establish(SCardScope.System))
+			{
+				using (var isoReader = new IsoReader(ctx
+											, this.m_nfc.ReaderName
+											, SCardShareMode.Shared
+											, SCardProtocol.Any
+											, false))
+				{
+
+					var apdu = new CommandApdu(IsoCase.Case2Short
+									, isoReader.ActiveProtocol)
+					{
+						CLA = 0x00, // Class
+						Instruction = InstructionCode.GetChallenge,
+						P1 = 0x00, // Parameter 1
+						P2 = 0x00, // Parameter 2
+						Le = 0x08 // Expected length of the returned data
+					};
+
+					var response = isoReader.Transmit(apdu);
+					Console.WriteLine("SW1 SW2 = {0:X2} {1:X2}", response.SW1, response.SW2);
+					// ..
+				}
+			}
+		}
+
+		/// <summary>
+		/// 리더기의 연결된 정보를 읽는다.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnGetData_Click(object sender, EventArgs e)
+		{
+			using (var context = ContextFactory.Instance.Establish(SCardScope.System))
+			{
+				using (var rfidReader = context.ConnectReader(
+											this.m_nfc.ReaderName
+											, SCardShareMode.Shared
+											, SCardProtocol.Any))
+				{
+					var apdu = new CommandApdu(IsoCase.Case2Short, rfidReader.Protocol)
+					{
+						CLA = 0xFF,
+						Instruction = InstructionCode.GetData,
+						P1 = 0x00,
+						P2 = 0x00,
+						Le = 0 // We don't know the ID tag size
+					};
+
+					using (rfidReader.Transaction(SCardReaderDisposition.Leave))
+					{
+						var sendPci = SCardPCI.GetPci(rfidReader.Protocol);
+						var receivePci = new SCardPCI(); // IO returned protocol control information.
+
+						var receiveBuffer = new byte[256];
+						var command = apdu.ToArray();
+
+						var bytesReceived = rfidReader.Transmit(
+							sendPci, // Protocol Control Information (T0, T1 or Raw)
+							command, // command APDU
+							command.Length,
+							receivePci, // returning Protocol Control Information
+							receiveBuffer,
+							receiveBuffer.Length); // data buffer
+
+						var responseApdu =
+							new ResponseApdu(receiveBuffer, bytesReceived, IsoCase.Case2Short, rfidReader.Protocol);
+
+						this.LogAdd(string.Format("SW1: {0:X2}, SW2: {1:X2}\n Uid: {2}"
+								, responseApdu.SW1,
+							responseApdu.SW2,
+							responseApdu.HasData 
+								? BitConverter.ToString(responseApdu.GetData()) 
+								: "No uid received"));
+					}
+				}
+			}//end using context
+		}
+
+		private void btnSetData_Click(object sender, EventArgs e)
+		{
+
+		}
+
+
+		#region Custom Command - Block 0
+
+		/// <summary>
+		/// 권한 요청
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void btnAuthBlock1_Click(object sender, EventArgs e)
+		{
+			using (var context = ContextFactory.Instance.Establish(SCardScope.System))
+			{
+				using (var isoReader 
+						= new IsoReader(context: context
+										, readerName: this.m_nfc.ReaderName
+										, mode: SCardShareMode.Shared
+										, protocol: SCardProtocol.Any
+										, releaseContextOnDispose: false))
+				{
+					var card = new MifareCard(isoReader);
+
+
+
+					var authSuccessful 
+						= card.Authenticate(
+							MSB
+							, LSB
+							, KeyType.KeyA
+							, 0x00);
+					if (false == authSuccessful)
+					{
+						this.LogAdd(string.Format("- Error - AUTHENTICATE failed."));
+					}
+					else
+					{//인증 성공
+						this.labInfo.Text = "Authentication : success";
+						this.labInfo.BackColor = Color.FromArgb(255, 80, 240, 180);
+						this.LogAdd(string.Format("AUTHENTICATE success."));
+					}
+
+
+				}
+			}
+		}
+
+		private void btnBlock0ReadBinary_Click(object sender, EventArgs e)
+		{
+			using (var context = ContextFactory.Instance.Establish(SCardScope.System))
+			{
+				using (var isoReader
+						= new IsoReader(context: context
+										, readerName: this.m_nfc.ReaderName
+										, mode: SCardShareMode.Shared
+										, protocol: SCardProtocol.Any
+										, releaseContextOnDispose: false))
+				{
+					MifareCard card = new MifareCard(isoReader);
+
+					byte[]? result = card.ReadBinary(MSB, LSB, 16);
+					if (null != result)
+					{
+						this.LogAdd(
+							string.Format("Result (before BINARY UPDATE): {0}"
+											, BitConverter.ToString(result)));
+					}
+					else
+					{
+						this.labInfo.Text = "Authentication : none";
+						this.labInfo.BackColor = Color.FromArgb(255, 160, 0, 184);
+						this.LogAdd(string.Format("- Error - Read Binary failed. - 인증을 다시 받으면 해결 될 수 있음."));
+					}
+				}
+			}//end using context
+
+		}
+
+		private void btnBlock0UpdateBinary_Click(object sender, EventArgs e)
+		{
+			string sData = this.txtUpdateBinary.Text;
+			//a타입 기준 1블럭은 16bytes 이다.(ACR122U 기준)
+			byte[] byteData_Write 
+				= Get_Left(ASCIIEncoding.Default.GetBytes(sData), 16);
+
+			
+
+			using (var context = ContextFactory.Instance.Establish(SCardScope.System))
+			{
+				using (var isoReader
+						= new IsoReader(context: context
+										, readerName: this.m_nfc.ReaderName
+										, mode: SCardShareMode.Shared
+										, protocol: SCardProtocol.Any
+										, releaseContextOnDispose: false))
+				{
+					var card = new MifareCard(isoReader);
+
+					var updateSuccessful = card.UpdateBinary(MSB, LSB, byteData_Write);
+
+					if (true == updateSuccessful)
+					{//
+						this.LogAdd(string.Format("Update Binary : success "));
+					}
+					else
+					{
+						this.labInfo.Text = "Authentication : none";
+						this.labInfo.BackColor = Color.FromArgb(255, 160, 0, 184);
+						this.LogAdd(string.Format("- Error - Update Binary : failed. - 인증을 다시 받으면 해결 될 수 있음."));
+					}
+				}
+			}//end using context
+		}
+		#endregion
+
+		/// <summary>
+		/// 왼쪽부터 지정한 길이만큼 데이터를 가지고 온다.
+		/// </summary>
+		/// <param name="byteA"></param>
+		/// <param name="nLength"></param>
+		/// <returns></returns>
+		public static byte[] Get_Left(byte[] byteA, int nLength)
+		{
+			byte[] byteCut = new byte[nLength];
+
+			int nLeng = nLength;
+			if (byteA.Length <= nLength)
+			{//데이터가 입력된 길이보다 작다.
+
+				//데이터 길이만큼만 사용한다.
+				nLeng = byteA.Length;
+			}
+
+			Array.Copy(byteA, 0, byteCut, 0, nLeng);
+
+			return byteCut;
+		}
+
+		#region Block Command
+
+		#endregion
+	}
+}
