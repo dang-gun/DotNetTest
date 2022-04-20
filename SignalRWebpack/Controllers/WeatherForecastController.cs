3559988ceb58d432fd287bc5037743be7a7e98d6
dang-gun <c1dong8@gmail.com>
2022-04-20 11:42:02 +0900

    프로젝트 제거

--- SignalRWebpack/Controllers/WeatherForecastController.cs
+++ SignalRWebpack/Controllers/WeatherForecastController.cs
@@ -6,10 +6,20 @@ namespace SignalRWebpack.Controllers
 	[Route("[controller]")]
 	public class WeatherForecastController : ControllerBase
 	{
-		private static readonly string[] Summaries = new[]
-		{
-		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-	};
+		private static readonly string[] Summaries 
+			= new[]
+			{
+				"Freezing"
+				, "Bracing"
+				, "Chilly"
+				, "Cool"
+				, "Mild"
+				, "Warm"
+				, "Balmy"
+				, "Hot"
+				, "Sweltering"
+				, "Scorching"
+			};
 
 		private readonly ILogger<WeatherForecastController> _logger;
 
